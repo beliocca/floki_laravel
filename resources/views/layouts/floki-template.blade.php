adda943716ffba2edfb29fd6c542a1a1d43b8a8a
solblack <solbeor@hotmail.com>
2019-06-24 19:51:11 -0300

    cambios en style.css y template.blade

--- resources/views/layouts/floki-template.blade.php
+++ resources/views/layouts/floki-template.blade.php
@@ -379,24 +379,27 @@
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-<script type="text/javascript">
-// Collapse accordion every time dropdown is shown
-$('.dropdown-collapse').on('show.bs.dropdown', function (event) {
-  var collapse = $(this).find($(this).data('accordion'));
-  collapse.find('.panel-collapse.in').collapse('hide');
-});
+      <script type="text/javascript">
+      // Collapse every time dropdown is shown
+      $('.dropdown-collapse').on('show.bs.dropdown', function (event) {
+        var collapse = $(this).find($(this).data('accordion'));
+        collapse.find('.panel-collapse.in').collapse('hide');
+      });
 
-// Prevent dropdown to be closed when we click on an accordion link
-$('.dropdown-collapse').on('click', 'a[data-toggle="collapse"]', function (event) {
-  event.preventDefault();
-  event.stopPropagation();
-  $($(this).data('parent')).find('.panel-collapse.in').collapse('hide');
-  $($(this).attr('href')).collapse('show');
-})
-</script>
+      // Prevent dropdown to be closed when we click on an collapse link
+      $('.dropdown-collapse').on('click', 'a[data-toggle="collapse"]', function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        $($(this).data('parent')).find('.panel-collapse.in').collapse('hide');
+        $($(this).attr('href')).collapse('show');
+      })
+      </script>
 
       <!--  scripts de Javascript-->
 
+        <script>
+      
+        </script>
 
   </body>
 </html>
